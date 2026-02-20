832e170d3b13e894919a3d3c7c8307e16e36b2ad
ValeAngeletti <valeangelett11@gmail.com>
2026-02-19 21:03:19 -0300

    feat(web): se agrego seccion funcionamiento al home

--- horsetrust/components/home/HomeContainer.tsx
+++ horsetrust/components/home/HomeContainer.tsx
@@ -1,5 +1,6 @@
 import BannerContainer from "./BannerContainer"
 import FeaturedSection from "./FeaturedSection"
+import OperationSection from "./OperationSection"
 import CategoriesContainer from "./CategoriesContainer"
 import ContactSection from "./ContactSection"
 
@@ -8,6 +9,7 @@ export default function HomeContainer(){
         <div>
             <BannerContainer/>
             <FeaturedSection/>
+            <OperationSection />
             <CategoriesContainer/>
             <ContactSection/>
         </div>
