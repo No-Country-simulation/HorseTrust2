759ba73616a7af54b38b80adc98ba7c6ae8527db
ValeAngeletti <valeangelett11@gmail.com>
2026-02-20 13:45:15 -0300

    feat(web): implementar ruouting interno y links de navegacion

---
+++ horsetrust/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactPage() {
+  return (
+    <section className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl">Contacto</h1>
+    </section>
+  );
+}
