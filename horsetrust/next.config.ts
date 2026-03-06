084ee390e0fd55bc2d577ab96458d31a08cd5698
ValeAngeletti <valeangelett11@gmail.com>
2026-03-06 07:41:25 -0300

    fix: eliminacion icono Next

--- horsetrust/next.config.ts
+++ horsetrust/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   serverExternalPackages: ["typeorm", "reflect-metadata", "pg"],
+
+  devIndicators: false,
 };
 
 export default nextConfig;
